54e9e687a71df4fdea8e80a980537bb1ee396fe6
Suresh Chaudhary <hello@suresh.im>
2026-03-18 14:20:37 +0530

    Add support for variables in the prompt messages backend

--- transports/bifrost-http/handlers/prompts.go
+++ transports/bifrost-http/handlers/prompts.go
@@ -115,25 +115,28 @@ type CreateVersionRequest struct {
 	ModelParams   tables.ModelParams     `json:"model_params"`
 	Provider      string                 `json:"provider"`
 	Model         string                 `json:"model"`
+	Variables     tables.PromptVariables  `json:"variables,omitempty"`
 }
 
 // CreateSessionRequest represents the request body for creating a session
 type CreateSessionRequest struct {
-	Name        string                 `json:"name"`
-	VersionID   *uint                  `json:"version_id,omitempty"`
-	Messages    []tables.PromptMessage `json:"messages,omitempty"`
-	ModelParams tables.ModelParams     `json:"model_params"`
-	Provider    string                 `json:"provider"`
-	Model       string                 `json:"model"`
+	Name        string                  `json:"name"`
+	VersionID   *uint                   `json:"version_id,omitempty"`
+	Messages    []tables.PromptMessage  `json:"messages,omitempty"`
+	ModelParams tables.ModelParams      `json:"model_params"`
+	Provider    string                  `json:"provider"`
+	Model       string                  `json:"model"`
+	Variables   tables.PromptVariables  `json:"variables,omitempty"`
 }
 
 // UpdateSessionRequest represents the request body for updating a session
 type UpdateSessionRequest struct {
-	Name        string                 `json:"name"`
-	Messages    []tables.PromptMessage `json:"messages"`
-	ModelParams tables.ModelParams     `json:"model_params"`
-	Provider    string                 `json:"provider"`
-	Model       string                 `json:"model"`
+	Name        string                  `json:"name"`
+	Messages    []tables.PromptMessage  `json:"messages"`
+	ModelParams tables.ModelParams      `json:"model_params"`
+	Provider    string                  `json:"provider"`
+	Model       string                  `json:"model"`
+	Variables   tables.PromptVariables  `json:"variables,omitempty"`
 }
 
 // RenameSessionRequest represents the request body for renaming a session
@@ -610,6 +613,7 @@ func (h *PromptsHandler) createVersion(ctx *fasthttp.RequestCtx) {
 		ModelParams:   req.ModelParams,
 		Provider:      req.Provider,
 		Model:         req.Model,
+		Variables:     req.Variables,
 		Messages:      messages,
 	}
 
@@ -806,6 +810,7 @@ func (h *PromptsHandler) createSession(ctx *fasthttp.RequestCtx) {
 		ModelParams: req.ModelParams,
 		Provider:    req.Provider,
 		Model:       req.Model,
+		Variables:   req.Variables,
 		Messages:    messages,
 	}
 
@@ -861,6 +866,7 @@ func (h *PromptsHandler) updateSession(ctx *fasthttp.RequestCtx) {
 	session.ModelParams = req.ModelParams
 	session.Provider = req.Provider
 	session.Model = req.Model
+	session.Variables = req.Variables
 
 	// Update messages
 	var messages []tables.TablePromptSessionMessage
@@ -1012,12 +1018,22 @@ func (h *PromptsHandler) commitSession(ctx *fasthttp.RequestCtx) {
 		})
 	}
 
+	// Copy variable keys from session with empty values for the version
+	var versionVars tables.PromptVariables
+	if len(session.Variables) > 0 {
+		versionVars = make(tables.PromptVariables, len(session.Variables))
+		for key := range session.Variables {
+			versionVars[key] = ""
+		}
+	}
+
 	version := &tables.TablePromptVersion{
 		PromptID:      session.PromptID,
 		CommitMessage: req.CommitMessage,
 		ModelParams:   session.ModelParams,
 		Provider:      session.Provider,
 		Model:         session.Model,
+		Variables:     versionVars,
 		Messages:      messages,
 	}
 
